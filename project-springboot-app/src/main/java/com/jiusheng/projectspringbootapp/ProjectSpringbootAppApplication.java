b6624d4c6c32dc1dc9a9109d7dbe97f07a324579
lianjiusheng <lianjiusheng@126.com>
2018-07-18 21:59:30 +0800

    项目模板初始化
    项目模板初始化

---
+++ project-springboot-app/src/main/java/com/jiusheng/projectspringbootapp/ProjectSpringbootAppApplication.java
@@ -0,0 +1,12 @@
+package com.jiusheng.projectspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringbootAppApplication.class, args);
+    }
+}
